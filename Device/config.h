0b3bbf610a1a472783f37252ea3f2eb933878e8a
wowa <waldemarstell@gmx.de>
2018-10-20 00:59:35 +0200

    initial commit

---
+++ Device/config.h
@@ -0,0 +1,11 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. 
+
+// Interval time(ms) for sending message to IoT Hub
+#define INTERVAL 30000
+
+#define MESSAGE_MAX_LEN 256
+
+#define TEMPERATURE_ALERT_HIGHER 40
+
+#define TEMPERATURE_ALERT_LOWER 0
